f1f784ff55061960c99eec2c0deb9b78f45d8754
Valentine <hi@valbuilds.xyz>
2025-04-11 17:30:43 -0500

    Initial Commit

---
+++ WMATA Fare Thingymajig/WMATA_Fare_ThingymajigApp.swift
@@ -0,0 +1,17 @@
+//
+//  WMATA_Fare_ThingymajigApp.swift
+//  WMATA Fare Thingymajig
+//
+//  Created by Valentine Gacke on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct WMATA_Fare_ThingymajigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
